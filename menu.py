1e66472190c5d2199103cd3cc64ac798c428fdfb
JimNastic316 <JimNastic316@gmail.com>
2021-04-21 08:48:29 -0400

    updated menu
    fine tuned, added notes

--- menu.py
+++ menu.py
@@ -8,4 +8,14 @@ menu = { '1': "one",
 
 print("Please choose (0 to quit)")
 for choice in menu:
-    print(f"\t {menu[choice]}")
+    print(f"\t {choice} - {menu[choice]}")
+
+while True:
+    choice = input()
+
+    if choice == '0':
+        print("Good bye")
+        break
+
+    if choice in menu.keys():
+        print(f"You chose {choice}")
